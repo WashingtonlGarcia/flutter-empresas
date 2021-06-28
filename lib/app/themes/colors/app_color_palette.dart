592035d1b5175f4b580b5c6883822170e1162f75
Washington Luiz <washingtonlgarcia@gmail.com>
2021-06-28 14:03:36 -0300

    feat: typography, color palette were created.

---
+++ lib/app/themes/colors/app_color_palette.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart' show Color;
+
+class AppColorPalette {
+  static Color get grape => const Color(0xFF8227B9);
+
+  static Color get byzantine => const Color(0xFFA71D9D);
+
+  static Color get mediumVioletRed => const Color(0xFFAE1A7B);
+
+  static Color get razzmatazz => const Color(0xFFE01E69);
+
+  static Color get platinum => const Color(0xFFE5E5E5);
+
+  static Color get graniteGray => const Color(0xFF666666);
+
+  static Color get piggyPink => const Color(0xFFFBDBE7);
+
+  static Color get cultured => const Color(0xFFF5F5F5);
+
+  static Color get electricRed => const Color(0xFFE00000);
+
+  static Color get white => const Color(0xFFFFFFFF);
+
+  static Color get black => const Color(0xFF000000);
+
+  static Color get moonstoneBlue => const Color(0xFF79BBCA);
+
+  static Color get ruddyPink => const Color(0xFFEB9797);
+
+  static Color get darkSeaGreen => const Color(0xFF90BB81);
+}
